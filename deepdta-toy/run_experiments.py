06126ed5ec8d12ed9e70a8c1324c7711963a32fb
Ashutosh Ghimire <ashutosh@jbnu.ac.kr>
2021-12-21 14:30:19 +0900

    multi process

--- deepdta-toy/run_experiments.py
+++ deepdta-toy/run_experiments.py
@@ -363,15 +363,17 @@ def general_nfold_cv(XD, XT,  Y, label_row_inds, label_col_inds, prfmeasure, run
 
                 for param3ind in range(len(paramset3)):
                     param3value = paramset3[param3ind]
+                    mirrored_strategy = tf.distribute.MirroredStrategy() 
+                    with mirrored_strategy.scope():
 
-                    gridmodel = runmethod(FLAGS, param1value, param2value, param3value)
-                    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=15)
-                    gridres = gridmodel.fit(([np.array(train_drugs),np.array(train_prots) ]), np.array(train_Y), batch_size=batchsz, epochs=epoch, 
-                            validation_data=( ([np.array(val_drugs), np.array(val_prots) ]), np.array(val_Y)),  shuffle=False, callbacks=[es] ) 
+                        gridmodel = runmethod(FLAGS, param1value, param2value, param3value)
+                        es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=15)
+                        gridres = gridmodel.fit(([np.array(train_drugs),np.array(train_prots) ]), np.array(train_Y), batch_size=batchsz, epochs=epoch, 
+                                validation_data=( ([np.array(val_drugs), np.array(val_prots) ]), np.array(val_Y)),  shuffle=False, callbacks=[es] ) 
 
 
-                    predicted_labels = gridmodel.predict([np.array(val_drugs), np.array(val_prots) ])
-                    loss, rperf2 = gridmodel.evaluate(([np.array(val_drugs),np.array(val_prots) ]), np.array(val_Y), verbose=0)
+                        predicted_labels = gridmodel.predict([np.array(val_drugs), np.array(val_prots) ])
+                        loss, rperf2 = gridmodel.evaluate(([np.array(val_drugs),np.array(val_prots) ]), np.array(val_Y), verbose=0)
                     rperf = prfmeasure(val_Y, predicted_labels)
                     rperf = rperf[0]
 
